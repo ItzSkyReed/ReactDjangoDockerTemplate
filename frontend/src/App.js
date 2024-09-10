f68e58158e14421b3337540afa9683243e4fe0de
ItzSkyReed <prodamsky@gmail.com>
2024-09-10 21:47:38 +0500

    Branch without react start files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import {useState, useEffect} from 'react';
 
 function App() {
@@ -14,7 +12,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>An Awesome Blog </h1>
         <h3>On Django, React, Postgres, and Docker </h3>
 
